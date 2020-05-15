7fa53393c5bc9751da7a3a2cbf2e3ee6f04a1df5
saddamcrr7 <saddamcrr7@users.noreply.github.com>
2020-05-15 08:29:37 +0000

    deploy: 10508fc948c384fdba785c64523fcafd36e6f40b

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var testimonialsSlider = new Swiper('.o-testimonials__slider-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 28.5,
+    wrapperClass: 'o-testimonials__slider-wrapper',
+    slideClass: 'o-testimonials__slider-item',
+    slideActiveClass: 'o-testimonials__slider-item--slide-active',
+});
