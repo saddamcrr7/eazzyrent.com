2d3a5a086c6f3c46815a5fd55ce478bbb4d551af
Hossain Saddam <hossaincrr7@gmail.com>
2020-05-18 07:47:27 +0600

    update contact section

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,18 @@ var testimonialsSlider = new Swiper('.o-testimonials__slider-container', {
         clickable: true,
     }
 });
+
+
+var checkboxs = document.querySelectorAll('.c-checkbox');
+checkboxs.forEach(function (checkbox) {
+  var input = checkbox.querySelector('.c-checkbox__input');
+  input.addEventListener('click', function () {
+    checkbox.classList.forEach(function (cls) {
+      if (cls.indexOf("is-checked") !== -1) {
+        checkbox.classList.remove('is-checked');
+      } else {
+        checkbox.classList.add('is-checked');
+      }
+    });
+  });
+});
